2861087242584a80ae40517b1dfd4f4227265b04
Eric Honas <honased@gmail.com>
2022-03-13 19:20:06 -0500

    Remove submodule ref to HonasGame.

---
+++ HonasGame/HonasGame/Tiled/Entities/TiledTilesEntity.cs
@@ -0,0 +1,91 @@
+﻿using HonasGame.ECS;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HonasGame.Tiled.Entities
+{
+    public class TiledTilesEntity : Entity
+    {
+        private const uint FLIPPED_HORIZONTALLY = 0x80000000;
+        private const uint FLIPPED_VERTICALLY = 0x40000000;
+        private const uint FLIPPED_DIAGONALLY = 0x20000000;
+        private const uint FLIPPED_HEXAGONAL = 0x10000000;
+
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int TileWidth { get; private set; }
+        public int TileHeight { get; private set; }
+        private uint[] _tiles;
+        private List<TiledMap.Tileset> _tilesets;
+
+        public TiledTilesEntity(int w, int h, int tw, int th, uint[] tiles)
+        {
+            Width = w;
+            Height = h;
+            TileWidth = tw;
+            TileHeight = th;
+            _tiles = tiles;
+        }
+
+        internal void SetTilesets(List<TiledMap.Tileset> tilesets)
+        {
+            _tilesets = tilesets;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            string cachedTexStr = "";
+            Texture2D cachedTex = null;
+
+            for (int j = 0; j < Width * Height; j++)
+            {
+                uint tile = _tiles[j];
+
+                if (tile == 0) continue;
+
+                SpriteEffects effects = SpriteEffects.None;
+                bool flippedVertically = (tile & FLIPPED_VERTICALLY) > 0;
+                bool flippedHorizontally = (tile & FLIPPED_HORIZONTALLY) > 0;
+                tile &= ~(FLIPPED_HORIZONTALLY | FLIPPED_VERTICALLY | FLIPPED_DIAGONALLY | FLIPPED_HEXAGONAL);
+
+                if (flippedHorizontally) effects |= SpriteEffects.FlipHorizontally;
+                if (flippedVertically) effects |= SpriteEffects.FlipVertically;
+
+                // Find Tileset
+                for (int k = _tilesets.Count - 1; k >= 0; k--)
+                {
+                    var _set = _tilesets[k];
+                    if (_set.FirstGid <= tile)
+                    {
+                        tile -= (uint)_set.FirstGid;
+
+                        TiledTileset actualSet = Assets.AssetLibrary.GetAsset<TiledTileset>(_set.Source);
+
+                        if (cachedTexStr != actualSet.Image)
+                        {
+                            cachedTexStr = actualSet.Image;
+                            cachedTex = Assets.AssetLibrary.GetAsset<Texture2D>(actualSet.Image);
+                        }
+
+                        Vector2 pos = new Vector2((j % Width) * TileWidth, (j / Width) * TileHeight);
+                        Vector2 sourcePos = actualSet.GetTileLocation(tile);
+                        Rectangle sourceRect = new Rectangle((int)sourcePos.X, (int)sourcePos.Y, TileWidth, TileHeight);
+                        spriteBatch.Draw(cachedTex, pos, sourceRect, Color.White, 0.0f, Vector2.Zero, Vector2.One, effects, 0.0f);
+                        break;
+                    }
+
+                }
+            }
+
+            base.Draw(gameTime, spriteBatch);
+        }
+
+        protected override void Cleanup()
+        {
+
+        }
+    }
+}
---
+++ HonasGame/HonasGame/Tiled/TiledObject.cs
@@ -0,0 +1,40 @@
+﻿using HonasGame.JSON;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HonasGame.Tiled
+{
+    public class TiledObject
+    {
+        private const uint FLIPPED_HORIZONTALLY = 0x80000000;
+        private const uint FLIPPED_VERTICALLY = 0x40000000;
+
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+        public bool FlippedHorizontally { get; private set; }
+        public bool FlippedVertically { get; private set; }
+
+        public TiledObject(JObject jObj)
+        {
+            X = (float)jObj.GetValue<double>("x");
+            Y = (float)jObj.GetValue<double>("y");
+            Width = (int)jObj.GetValue<double>("width");
+            Height = (int)jObj.GetValue<double>("height");
+            Id = (int)jObj.GetValue<double>("id");
+            Name = jObj.GetValue<string>("name");
+            Type = jObj.GetValue<string>("type");
+            if(jObj.CheckField("gid"))
+            {
+                int gid = (int)jObj.GetValue<double>("gid");
+                FlippedHorizontally = (gid & FLIPPED_HORIZONTALLY) > 0;
+                FlippedVertically = (gid & FLIPPED_VERTICALLY) > 0;
+            }
+        }
+    }
+}
